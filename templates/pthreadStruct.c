364cf967e1d02730de20fd816faeb394392d7d69
Brian G <justlostintime@gmail.com>
2025-04-20 15:04:16 -0700

    Updated for various bugs, template fixes, build fixes .. even more
    *BUG: Variaous bugs in template file, updated c build to link correctly
    *OPT: Added a wait no wait interface call to ShmThread
    *NEW: Added WaitComplete interface for ShmThread
    *OPT: Allow to write no messages to output to test overhead time Start End times
    *NEW: Build script in template directory for c and asm blobs
    *OPT: Completed SHM execte segment and ShmThread classes

--- templates/pthreadStruct.c
+++ templates/pthreadStruct.c
@@ -3,14 +3,14 @@
 
 #include <unistd.h>
 #include <sys/syscall.h>
+#include <pthread.h>
 
 typedef struct {
-  long result;               // since we use syscall to stop thread then input structur's first field will be result
+  long result;               // since we use syscall to stop thread then input structure's first field will be result
   long count;                // number of times to loop
   char *msg;                 // message to be displayed
   long writeLen;             // The returned number of characters to write
 } mys;
-
 
 void showMsg(char *msg, int length);
 int stringlength(char *msg);
@@ -25,34 +25,35 @@ void threadEntry(mys *data) {
   //  void * slen  = &stringlength;
 
   int strit = stringlength(data->msg);
-  
+
   data->writeLen = strit;
 
   for(y=1; y <= data->count; y++) {
     data->result=y;
 
     showMsg(data->msg, strit); // Write the message out using the system call
-   
+
   }
 
   // Exit the program using a system call
   ThreadExit(84);
-  
+  //pthread_exit(data);
+
 }
 
 // Exit the thread right away
 void ThreadExit(int result){
-   unsigned long syscall_nr = SYS_exit;
-   long exit_status = result;
+  unsigned long syscall_nr = SYS_exit;
+  long exit_status = result;
 
   asm ("movq %0, %%rax\n"
     "movq %1, %%rdi\n"
     "syscall"
-    : /* output parameters, we aren't outputting anything, no none */
-    /* (none) */
-    : /* input parameters mapped to %0 and %1, repsectively */
+    : // output parameters, we aren't outputting anything, no none
+    // (none)
+    : // input parameters mapped to %0 and %1, repsectively
     "m" (syscall_nr), "m" (exit_status)
-    : /* registers that we are "clobbering", unneeded since we are calling exit */
+    : // registers that we are "clobbering", unneeded since we are calling exit
   "rax", "rdi");
 }
 
@@ -67,11 +68,11 @@ void showMsg(char *msg, int length){
     "movq %2, %%rdx\n"
     "movq %3, %%rdi\n"
     "syscall"
-    : /* output parameters, we aren't outputting anything, no none*/
-      /* (none) */
-    : /* input parameters mapped to %0 and %1, repsectively */
+    : // output parameters, we aren't outputting anything, no none
+    // (none)
+    : // input parameters mapped to %0 and %1, repsectively
     "m" (syscall_nr), "m" (msg), "m" (len_parm), "m" (stdout_nr)
-    : /* registers that we are "clobbering", unneeded since we are calling exit */
+    : // registers that we are "clobbering", unneeded since we are calling exit
   "rax", "rdi", "rsi", "rdx");
 
   return;
@@ -87,4 +88,3 @@ int stringlength(char *msg) {
   }
   return count;
 }
-
